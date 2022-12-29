2be8b56601c0b79b3f03bde81c2f1e24eb6696a4
alibaba0010 <zakariyahali100@gmail.com>
2022-12-29 16:40:57 +0100

    Minor changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,14 @@
 import express from "express";
 import { v4 as uuidv4, v4 } from "uuid";
 
-
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static( "public"));
+app.use(express.static("public"));
 app
   .get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
   })
   .get("/:room", (req, res, next) => {
-    res.render("room", { roomId: req.params.room });
+    res.status().render("room", { roomId: req.params.room });
   });
 export default app;

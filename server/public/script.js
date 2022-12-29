367f7d5e5296b85e11adaefd577e09d5eb2b6ce9
alibaba0010 <zakariyahali100@gmail.com>
2022-12-29 14:54:24 +0100

    using peerjs

--- server/public/script.js
+++ server/public/script.js
@@ -1,6 +1,62 @@
-const socket = io("/");
+const video = document.createElement("video");
 
-socket.emit("join-room", roomId, 10);
-socket.on("user-connected", (userId) => {
+const videoGrid = document.getElementById("video_grid");
+
+const socket = io("/");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
 });
- 
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on("call",  (call) => {
+       call.answer(stream);
+
+      call.on("stream", (userVideoStream) => {
+        console.log("In stream");
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+       connectNewUser(userId, stream);
+    });
+  });
+myPeer.on("open", (peerId) => {
+  socket.emit("join-room", roomId, peerId);
+});
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+socket.on("user-disconnected", (userId)=>{
+   
+})
+
+const connectNewUser =  (userId, stream) => {
+  const call =  myPeer.call(userId, stream);
+
+  call.on("stream", (userVideoStream) => {
+    console.log("In check");
+
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+};
--- server/src/server.js
+++ server/src/server.js
@@ -4,10 +4,13 @@ import { Server } from "socket.io";
 const server = createServer(app);
 
 const io = new Server(server);
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
   });
 });
 server.listen(3000, () => console.log("Server listening at port 3000"));
